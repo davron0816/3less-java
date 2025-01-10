df0de7535b8c3d1af486614a2cf943397e47dc52
davron0816 <davronergawev0816@gmail.com>
2025-01-10 22:58:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let user = prompt  `String haqida malumotni kirgizng`;
+let user2 = prompt  `Number typedagi malumotni kirgizning`;
+let user3 = prompt `Boolen typedagi malumotni kiriting`;
+let message = `Hamma malumotlarni kirgizganizga 
+ishonchingiz komilmi ?`
+alert(
+    `Siz kiritgan ${user} so'zi string typega tegishli 
+    Siz kiritgan ${user2} so'zi number typega tegishli
+    Siz kiritgan ${user3} so'zi boolean typega tegishli
+    Malumotlaringizni togri kiritganingizdan minnatdormiz😊`
+  );
+
